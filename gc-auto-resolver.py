a46a2052a56457452b45f27ec057a2ded3c0c961
Carroll, Brian <bcarroll@arifleet.com>
2021-08-06 11:57:33 -0400

    first push

---
+++ gc-auto-resolver.py
@@ -0,0 +1,201 @@
+import json
+import yaml
+import requests
+import logging
+import requests_cache
+from datetime import datetime, timedelta
+from argparse import ArgumentParser
+from pyaml_env import parse_config
+from threat_feeds.feed import Feed
+
+
+def load_config(path="config.yml"):
+    """
+    Loads the configuration file for the application
+    and returns a configuration object for consumption in
+    other areas
+    """
+    config_error = False
+    config = parse_config(path)
+
+    return config
+
+
+def load_feeds(feed_configs):
+    """
+    Initiates a Feed object for each threat feed configured
+    """
+    feeds = [Feed(feed_configs[f]) for f in feed_configs if feed_configs[f]['disabled'] == False]
+    return feeds
+
+
+def gc_authenticate(management_url, username, password):
+    """
+    Authenticates to the Guardicore API
+    """
+
+    s = requests.Session()
+
+    auth_body = {
+        "username": username,
+        "password": password
+    }
+
+    headers = {
+        "Content-Type": "application/json"
+    }
+
+    access_token = None
+
+    response = s.post(f"https://{management_url}/api/v3.0/authenticate", data=json.dumps(auth_body), headers=headers)
+    if response.status_code == 200:
+        access_token = response.json()["access_token"]
+
+    return access_token
+
+def gc_get_incidents(management_url, access_token, tags=[]):
+    """
+    Fetches a list of un-acknowledged incidents from the last 24 hours
+    """
+    tag_list = ",".join(tags)
+    from_time = int((datetime.now() - timedelta(hours=24)).timestamp()) * 1000
+    to_time = int(datetime.now().timestamp()) * 1000
+
+    with requests.Session() as s:
+        s.headers.update({
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {access_token}"
+        })
+        url = f"https://{management_url}/api/v3.0/incidents?tag={tag_list}&tag__not=Acknowledged&from_time={from_time}&to_time={to_time}&limit=500"
+        response = s.get(url)
+        if response.status_code == 200:
+            data = response.json()
+    return data['objects']
+
+
+def gc_tag_incident(management_url, access_token, incident_id, tags=[]):
+    """
+    Tags an incident with information and Acknowledges it
+    identifying that it was resolved for certain reasons
+    automatically
+    """
+
+    """ From Devtools response - Undocumented API endpoint
+    POST
+    https://cus-2782.cloud.guardicore.com/api/v3.0/incidents/acknowledge
+    {"ids":["e9fde08e-c52a-469e-971d-ab15f8ec672f"],"negate_args":null}
+    """
+
+    """ From Devtools response - Undocumented API endpoint
+    POST
+    https://cus-2782.cloud.guardicore.com/api/v3.0/incidents/tag
+    {"action":"add","tag_name":"Test","negate_args":null,"ids":["91d5531d-5915-477a-ae95-1af6981df625"]}
+    """
+
+    with requests.Session() as s:
+        s.headers.update({
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {access_token}"
+        })
+
+        # Assign all the tags
+        for tag in tags:
+            data = {
+                "action": "add",
+                "tag_name": tag,
+                "negate_args": None,
+                "ids": [incident_id]
+            }
+            s.post(f"https://{management_url}/api/v3.0/incidents/tag", data=json.dumps(data))
+
+        # Acknowledge the incident
+        data = {
+            "ids": [incident_id],
+            "negate_args": None
+        }
+        s.post(f"https://{management_url}/api/v3.0/incidents/acknowledge", data=json.dumps(data))
+
+    return
+
+
+def vt_lookup(ip):
+    """
+    Checks the VirusTotal API for threat intel on a given IP address
+    """
+    raise NotImplemented
+
+def gn_lookup(ip):
+    """
+    Checks the Greynoise API for threat intel on a given IP address
+    """
+    raise NotImplemented
+
+
+def enrich_incident(incident, minimum_hits, feeds=[]):
+    source = incident["source_asset"]
+    destination = incident["destination_asset"]
+
+    ip = None
+    total_feeds = len(feeds)
+    found_in = 0
+    feed_names = []
+
+    if destination['is_inner'] == False:
+        ip = destination['ip']
+
+    if source['is_inner'] == False:
+        ip = source['ip']
+
+    if ip:
+        for feed in feeds:
+            found =  feed.check_ip(ip)
+            if found:
+                found_in += 1
+                feed_names += [f"External threat list: {feed.name}"]
+    logging.info(f"{ip} found in {found_in}/{total_feeds} threat feeds.")
+    
+    # If the rule stipulates a certain number of hits for threat feeds and we meet or exceed 
+    # that threshold, automatically resolve the alarm
+    if found_in >= minimum_hits:
+        logging.info(f"Feed threshold crossed for {ip} automatically resolving incident.")
+        return True, feed_names
+    return False, feed_names
+
+
+if __name__ == "__main__":
+
+    # Set the logging format
+    logging.basicConfig(
+        format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+    parser = ArgumentParser()
+    parser.add_argument('--config', help="The path to the configuration file", default="config.yml", required=False)
+
+    args = parser.parse_args()
+
+    config = load_config(path=args.config)
+
+    # Hook requests with the cache
+    if config['caching']['enabled']:
+        logging.info("Enabling caching for Requests with a {} second(s) expiration period".format(config['caching']['expiration']))
+        requests_cache.install_cache('gc-auto-resolve', backend=config['caching']['backend'], expire_after=config['caching']['expiration'])
+
+    logging.info("Authenticating to Guardicore")
+    access_token = gc_authenticate(**config['guardicore'])
+
+    feeds = load_feeds(config['feeds'])
+    
+    for rule in config['rules']:
+        rule_config = config['rules'][rule]
+        logging.info(f"Running rule \"{rule}\"")
+
+        incidents = gc_get_incidents(config['guardicore']['management_url'], access_token, tags=rule_config['tags'])
+        if len(incidents) > 0:
+            logging.info("Processing {} incidents".format(len(incidents)))
+        
+            for incident in incidents:
+
+                if rule_config['type'] == "threat_enrich":
+                    threshold_exceeded, feed_names = enrich_incident(incident, rule_config['minimum_hits'], feeds=feeds)
+                    if threshold_exceeded:
+                        gc_tag_incident(config['guardicore']['management_url'], access_token, incident['id'], tags=feed_names+rule_config['resolution_tags'])
---
+++ threat_feeds/feed.py
@@ -0,0 +1,109 @@
+import dns.resolver
+import requests
+import re
+import requests_cache
+from requests import exceptions as rexp
+from netaddr import IPSet, IPNetwork, IPAddress
+
+
+class Feed(object):
+    '''
+    Creates a Feed object that allows for easier interaction with each
+    individual blacklist feed
+    '''
+    def __init__(self, data):
+        '''
+        Initializes the Feed object
+        :param data: A JSON formatted string that builds the object
+        '''
+        self.__dict__.update(data)
+
+    def check_ip(self, ip, options=None, *args, **kwargs):
+        if self.type == "list":
+            return self.check_ip_list(ip, options, *args, **kwargs)
+        elif self.type == "dns":
+            return self.check_ip_dns(ip, options, *args, **kwargs)
+
+
+    def check_ip_list(self, ip, options=None, *args, **kwargs):
+        '''
+        Checks a given IP against the blacklist
+        :param self:
+        :param ip: The IP address that we want to look for
+        :param options: The OptParse options from the main() function
+        :return Found|No Result:
+        '''
+
+        session = requests.Session()
+
+        # Skip the feed if it is disabled in config
+        if hasattr(self, "disabled") and self.disabled:
+            return "Skipped - Disabled"
+
+        # If the user supplied a proxy, set the proxy information for requests
+        if hasattr(options,'proxy') and options.proxy:
+            session.proxies = {"http": options.proxy, "https": options.proxy}
+            session.auth = HTTPProxyAuth(options.proxy_user, options.proxy_pass)
+
+        # Try to pull down the data from the feed URL
+        try:
+            result = session.get(self.url)
+            if result.status_code == 200:
+
+                # If the threat feed is in CIDR notation, pull all the listed subnets
+                # then see if the IP is a member of each one, if we find it stop checking
+                # If NOT CIDR notation, do the normal IP check
+                if self.format == "cidr":
+                    for cidr in [IPNetwork(cidr) for cidr in re.findall("((?:\d{1,3}\.){3}\d{1,3}(?:/\d\d?))", result.text)]:
+                        if IPAddress(ip) in cidr:
+                            return "Found"
+                    return "No Result"
+                else:
+                    matches = re.findall(ip, result.text)
+                if matches:
+                    return True
+                    #return "Found"
+                else:
+                    return False
+                    #return "No Result"
+            else:
+                cprint("[!] There was an issue attemping to connect to: {url}".format(url=self.url), RED)
+                return False
+        except rexp.ConnectionError as e:
+            cprint("[!] There was an issue attemping to connect to: {url}".format(url=self.url), RED)
+            return False
+
+    def check_ip_dns(self, ip, options=None, *args, **kwargs):
+        '''
+        Checks a given IP against a DNSBL (DNS Blacklist)
+        :param self:
+        :param ip:  The IP we are looking for
+        :param options:  The OptParse options from the main() function
+        :return Found|No Result|Timeout|No Answer
+        '''
+
+        try:
+        # Build our resolver
+            r = dns.resolver.Resolver()
+
+        # Create a reverse DNS query for the IP in question
+            query = '.'.join(reversed(str(ip).split("."))) + "." + self.url
+            r.timeout = 5
+            r.lifetime = 5
+
+            # Check for any A and TXT records matching the reverse record
+            answers = r.query(query, "A")
+            answers_txt = r.query(query, "TXT")
+
+            # Return a Found response if we have anythin in either list
+            if answers or answers_txt:
+                return "Found"
+
+        except dns.resolver.NXDOMAIN:
+            return "Not Found"
+        except dns.resolver.Timeout:
+            return "Timeout"
+        except dns.resolver.NoAnswer:
+            return "No Answer"
+        except dns.resolver.NoNameservers:
+            return "No Name Servers"
